f8608c6865089a5f01c02b1833f5d2fce2a5dce6
facuellarg <32692084+facuellarg@users.noreply.github.com>
2019-10-22 19:15:34 -0500

    Add files via upload

--- punto5.go
+++ punto5.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 	"math/rand"
 	"time"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 )
 
 var count int
@@ -12,6 +18,7 @@ var count int
 //swap  intercambia elementos de un arreglo
 func swap(arr []int, i, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
+	count++
 }
 
 //partition particiona el arreglo dejando a los menores
@@ -20,7 +27,7 @@ func partition(arr []int, low, hight int) int {
 	pivot := arr[hight]
 	i := low - 1
 	for j := low; j < hight-1; j++ {
-		count++
+
 		if arr[j] <= pivot {
 			i++
 			swap(arr, i, j)
@@ -39,44 +46,45 @@ func partitionR(arr []int, low, hight int) int {
 }
 
 //quickSort2 implementado de forma diferente, consume menos memoria
-func quickSort2(arr []int, low, hight int) {
+func quickSort(arr []int, low, hight int) {
 	if low < hight {
 		p := partitionR(arr, low, hight)
 
-		quickSort2(arr, low, p-1)
-		quickSort2(arr, p+1, hight)
+		quickSort(arr, low, p-1)
+		quickSort(arr, p+1, hight)
 
 	}
 }
 
 //quickSort implementado como dice el libro
-func quickSort(arr1 []int, low, hight int) []int {
-	if len(arr1) == 1 {
-		return arr1
-	}
+// func quickSort(arr1 []int, low, hight int) []int {
+// 	if len(arr1) == 1 {
+// 		return arr1
+// 	}
 
-	if len(arr1) != 0 {
-		idx := rand.Intn(len(arr1))
-		pivote := arr1[idx]
-		leftArr := make([]int, 0)
-		rightArr := make([]int, 0)
-		for _, v := range append(arr1[:idx], arr1[idx+1:]...) {
-			if v < pivote {
-				leftArr = append(leftArr, v)
-			} else {
-				rightArr = append(rightArr, v)
-			}
+// 	if len(arr1) != 0 {
+// 		idx := rand.Intn(len(arr1))
+// 		pivote := arr1[idx]
+// 		leftArr := make([]int, 0)
+// 		rightArr := make([]int, 0)
+// 		for _, v := range append(arr1[:idx], arr1[idx+1:]...) {
+// 			if v < pivote {
+// 				count++
+// 				leftArr = append(leftArr, v)
+// 			} else {
+// 				rightArr = append(rightArr, v)
+// 			}
 
-		}
-		count += len(arr1) - 1
-		leftArr = quickSort(leftArr, 0, len(leftArr))
-		rightArr = quickSort(rightArr, 0, len(leftArr))
-		return append((append(leftArr, pivote)), rightArr...)
-	}
+// 		}
 
-	return make([]int, 0)
+// 		leftArr = quickSort(leftArr, 0, len(leftArr))
+// 		rightArr = quickSort(rightArr, 0, len(leftArr))
+// 		return append((append(leftArr, pivote)), rightArr...)
+// 	}
 
-}
+// 	return make([]int, 0)
+
+// }
 
 // crea un nuevo array sin elementos repetidos
 func createArray(n int) []int {
@@ -90,6 +98,13 @@ func createArray(n int) []int {
 	}
 	return newArr
 }
+func createSortedArray(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = i
+	}
+	return arr
+}
 
 //contains verifica si un elemento esta en un arreglo dado
 func contains(arr []int, randInt int) bool {
@@ -102,33 +117,84 @@ func contains(arr []int, randInt int) bool {
 }
 
 func main() {
+	fileName := flag.String("fn", "barchart", "name for file to save barchart image")
+	maxSize := flag.Int("size", 10, "max size for the arrays")
+	iterations := flag.Int("it", 100000, "number of iterations")
+	flag.Parse()
+	*fileName += ".png"
 
 	rand.Seed(int64(time.Now().UnixNano()))
 	var evaluations, acum float64
+	size := *maxSize
+	var valorObtenido, valorEsperado, valorPeorCaso plotter.Values
 	fmt.Println("tam\tpromedio\tesperado\tworst-case")
-	for tam := 1; tam < 15; tam++ {
-		evaluations = 100000
+	init := 2
+	for tam := init; tam <= size; tam++ {
+		evaluations = float64(*iterations)
 		for i := 0; i < int(evaluations); i++ {
 			count = 0
 			arr1 := createArray(tam)
 			//fmt.Println(arr1)
-			quickSort2(arr1, 0, len(arr1)-1)
+			quickSort(arr1, 0, len(arr1)-1)
 			acum += float64(count)
-
 		}
-
-		x := math.Log(float64(tam))
-		y := math.Log(2.0)
-
-		esperado := 2 * float64(tam) * float64(x/y)
+		x := math.Log10(float64(tam))
+		esperado := 2 * float64(tam) * x
 		worstCase := tam * tam
+		valorObtenido = append(valorObtenido, acum/evaluations)
+		valorEsperado = append(valorEsperado, esperado)
+		valorPeorCaso = append(valorPeorCaso, float64(worstCase))
 		fmt.Printf("%3d\t%8.3f\t%8.3f\t%10d\n", tam, acum/evaluations, esperado, worstCase)
-		//fmt.Println(acum / evaluations)
+		acum = 0
 	}
 
-	count = 0
-	// arr1 := []int{5, 9, 3, 10, 11, 14, 8, 4, 17, 6}
-	// h := quickSort2(arr1, 0, len(arr1))
-	// fmt.Println(h, count)
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "QuickSort"
+	p.Y.Label.Text = "Cantidad de Intercambios"
+	w := vg.Points(10)
 
+	columnaObtenidos, err := plotter.NewBarChart(valorObtenido, w)
+	if err != nil {
+		panic(err)
+	}
+	columnaObtenidos.LineStyle.Width = vg.Length(0)
+	columnaObtenidos.Color = plotutil.DarkColors[2]
+	columnaObtenidos.XMin = float64(init)
+	columnaObtenidos.Offset = -w
+	columnaEsperados, err := plotter.NewBarChart(valorEsperado, w)
+	if err != nil {
+		panic(err)
+	}
+
+	columnaEsperados.LineStyle.Width = vg.Length(0)
+	columnaEsperados.Color = plotutil.DarkColors[0]
+	columnaEsperados.XMin = float64(init)
+	columnaPeorCaso, err := plotter.NewBarChart(valorPeorCaso, w)
+
+	if err != nil {
+		panic(err)
+	}
+
+	columnaPeorCaso.LineStyle.Width = vg.Length(0)
+	columnaPeorCaso.Color = plotutil.DarkColors[3]
+	columnaPeorCaso.XMin = float64(init)
+	columnaPeorCaso.Offset = w
+
+	p.Add(columnaObtenidos, columnaEsperados, columnaPeorCaso)
+	p.Legend.Add("Obtenido", columnaObtenidos)
+	p.Legend.Add("Esperado", columnaEsperados)
+	p.Legend.Add("Peor Caso", columnaPeorCaso)
+	p.Legend.Top = true
+	p.Legend.Left = true
+	p.X.Max = float64(size + 1)
+	p.Y.Max = valorPeorCaso[len(valorPeorCaso)-1]
+
+	p.X.Label.Text = "Tamaño del arreglo"
+	scale := vg.Length(w * 2)
+	if err := p.Save(16*scale*vg.Millimeter, 9*scale*vg.Millimeter, *fileName); err != nil {
+		panic(err)
+	}
 }
